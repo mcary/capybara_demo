04db6df26fc1a3971891e99ffbc44b483103922a
Marcel M. Cary <marcel@oreilly.com>
2012-10-11 16:05:06 -0700

    Add specs for Rx Speaker Cheeseboard

--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -17,8 +17,55 @@ describe "SSO" do
   end
 
   it "should sell widgets" do
+    #pending "We don't sell widets yet."
     visit "http://members.oreilly.com/widgets"
 
     page.should have_content("Buy a Widget") # Fails.  We don't sell those.
+  end
+end
+
+describe "RX homepage" do
+  describe "Speaker Cheeseboard" do
+    it "should show four speakers" do
+      using_wait_time 10 do # Ok if this loads slower / last on page
+        visit "http://strataconf.com/rx2012"
+        within ".cheese-board" do
+          should_load_four_speakers
+          page.all(".speaker img", :visible => true).size.should == 4
+        end
+      end
+    end
+
+    it "should show new speakers when the right arrow is clicked" do
+      using_wait_time 10 do # Ok if this loads slower / last on page
+        visit "http://strataconf.com/rx2012"
+        within ".cheese-board" do
+          should_load_four_speakers
+
+          initial_speakers = Hash.new
+          page.all(".speaker .caption", :visible => true).each do |capt|
+            initial_speakers[capt.text] = true
+            Rails.logger.debug "before: "+capt.text
+          end
+
+          find(".control-right").click
+          should_load_four_speakers
+
+          page.all(".speaker .caption", :visible => true).each do |capt|
+            Rails.logger.debug "after: "+capt.text
+            initial_speakers.should_not have_key(capt.text)
+          end
+        end
+      end
+    end
+
+    # This makes Capybara wait till it can find exactly four.
+    # Otherwise, it won't know how long to wait for JavaScript to
+    # insert/remove elements before searching for them.
+    def should_load_four_speakers
+      page.should have_css(([".speaker"] * 4).join(" + "))
+      page.should have_no_css(([".speaker"] * 5).join(" + "))
+    end
+
   end
 end
